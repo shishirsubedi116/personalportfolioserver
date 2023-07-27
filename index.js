595aab4a25bd01499b0af0cb47c45bd368c9a8ec
Shishir <shishirsubedi116@gmail.com>
2023-07-27 23:07:53 +0545

    Server Started

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/', require('./routers/order'))
+app.get('/', (req,res)=>{
+    return res.send('server started')
+})
 
 
 app.listen(port, ()=>{
